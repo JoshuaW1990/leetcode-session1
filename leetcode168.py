cf3cf15fc7addc9984a1bb04e69f9756af38464e
JoshuaW1990 <Jun.Wang@tufts.edu>
2017-07-06 11:33:18 -0400

    add solutions

--- leetcode168.py
+++ leetcode168.py
@@ -4,4 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        
+        ans = ''
+        tmp = n
+        while tmp > 0:
+            rem = (tmp - 1) % 26
+            ch = chr(ord('A') + rem)
+            ans = ch + ans
+            tmp = (tmp - 1) / 26
+        return ans
