9c763b274237a07c076b50b3541fa1b2c52dd0a1
JoshuaW1990 <Jun.Wang@tufts.edu>
2017-08-02 17:37:16 -0400

    add solutions

--- leetcode16.py
+++ leetcode16.py
@@ -9,4 +9,18 @@ class Solution(object):
         minDiff = float('inf')
         nums.sort()
         for i in xrange(len(nums) - 2):
-            if i
+            if i == 0 or nums[i] > nums[i - 1]:
+                left = i + 1
+                right = len(nums) - 1
+                while left < right:
+                    diff = target - (nums[left] + nums[right] + nums[i])
+                    if abs(diff) < minDiff:
+                        ans = nums[left] + nums[right] + nums[i]
+                        minDiff = abs(diff)
+                    if diff == 0:
+                        return ans
+                    elif diff > 0:
+                        left += 1
+                    else:
+                        right -= 1
+        return ans
---
+++ leetcode18.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        Starts from the 3Sum approach
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
